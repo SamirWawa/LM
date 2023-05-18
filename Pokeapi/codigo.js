9af1baacaca34b43e00848714cda0410d21077ad
SamirWawa <samir.wawa@iesdoctorbalmis.com>
2023-05-18 08:56:52 +0200

    Stats

--- Pokeapi/codigo.js
+++ Pokeapi/codigo.js
@@ -14,10 +14,20 @@ HTTP.ajax('GET', enlace).then((datosPokemon) => {
     
     let img = document.createElement("img");
     img.src = datosPokemon.sprites.front_default;
-    document.querySelector("#imagen").appendChild(img);
-    console.log(datosPokemon.stats[1].base_stat)
+    img.className = "imagen"
+    document.querySelector("#primer").insertBefore(img, document.querySelector("#primer").firstChild);
+
+    let vida = document.createElement("div");
+    vida.appendChild(document.createTextNode("Vida: "+datosPokemon.stats[0].base_stat))
+    document.querySelector("#primer").appendChild(vida)
+    let atck = document.createElement("div");
+    atck.appendChild(document.createTextNode("Ataque: "+datosPokemon.stats[1].base_stat))
+    document.querySelector("#primer").appendChild(atck)
+    let def = document.createElement("div");
+    def.appendChild(document.createTextNode("Defensa: "+datosPokemon.stats[2].base_stat))
+    document.querySelector("#primer").appendChild(def)
 }).catch((error) => {
-    console.log(error);
+    console.log(error)
 });
 
 function generarPokemon() {
