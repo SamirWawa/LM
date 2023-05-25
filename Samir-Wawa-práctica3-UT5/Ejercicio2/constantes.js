a3bf7628d9aed421fd48f341597ef1acf9dae845
SamirWawa <samir.wawa@iesdoctorbalmis.com>
2023-05-25 09:49:06 +0200

    Ejercicio2

--- Samir-Wawa-práctica3-UT5/Ejercicio2/constantes.js
+++ Samir-Wawa-práctica3-UT5/Ejercicio2/constantes.js
@@ -1,5 +1,5 @@
 'use strict'
 
-const API_BASE_URL = "https://api.themoviedb.org/3/movie/550?api_key=2ec78bb2ca62ae5f40bfb7476e293644"
+const API_BASE_URL = "url.json"
 const API_KEY = ""
 const IMAGE_BASE = ""
--- Samir-Wawa-práctica3-UT5/Ejercicio2/index.js
+++ Samir-Wawa-práctica3-UT5/Ejercicio2/index.js
@@ -1,1 +1,34 @@
 'use strict'
+
+function generarComida(){
+    HTTP.ajax('GET', API_BASE_URL).then((datosComida)=>{
+        datosComida.meals.forEach(element => {
+            let comida = document.createElement("div")
+            comida.appendChild(document.createTextNode(element.strMeal))
+            comida.className="Comida"
+
+            let imagen = document.createElement("img")
+            imagen.src = element.strMealThumb
+            imagen.className = "imagenComida"
+
+            let categoria = document.createElement("div")
+            categoria.appendChild(document.createTextNode("Tipo de comida: "+element.strCategory))
+
+            let pais = document.createElement("div")
+            pais.appendChild(document.createTextNode("Procedencia: "+element.strArea))
+            
+            let tutorial = document.createElement("a")
+            tutorial.href=element.strYoutube
+            tutorial.appendChild(document.createTextNode(element.strYoutube))
+
+            comida.appendChild(imagen)
+            comida.appendChild(categoria)
+            comida.appendChild(pais)
+            comida.appendChild(tutorial)
+
+            document.querySelector("main").appendChild(comida)
+        });
+        
+    })
+}
+generarComida()
